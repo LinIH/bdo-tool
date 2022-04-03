016f2f2fe188ef97824bd311b6793d9bedac3450
linih <nightsky257@gmail.com>
2022-04-04 02:52:47 +0800

    first commit

--- script/myscripts.js
+++ script/myscripts.js
@@ -4,17 +4,30 @@ $(document).ready(function(){
     $.getJSON('script/data.json', function(data){
         $.each(data, function(key, value){
             console.log('key=' + key);
-            console.log('value.tradeitem_name=' + value.tradeitem_name);
-            component_sentence = '<option>'+value.tradeitem_name+'</option>';
+            console.log('value.tradeitem_name=' + value[0].tradeitem_name);
+            component_sentence = '<option>'+value[0].tradeitem_name+'</option>';
             $('#select_tradeitem').append(component_sentence);
           });
     })
 });
 
+/*function getObjects(obj, key, val) {
+    var objects = [];
+    for (var i in obj) {
+        if (!obj.hasOwnProperty(i)) continue;
+        if (typeof obj[i] == 'object') {
+            objects = objects.concat(getObjects(obj[i], key, val));
+        } else if (i == key && obj[key] == val) {
+            objects.push(obj);
+        }
+    }
+    return objects;
+}*/
+
 $('button#add-item').click(function(){
     var option = $('#select_tradeitem option:selected');
     var option_val = option.val();
-    
+    var 
     var newdiv =
     '<div class="ui fluid card '+option_val+'">'+
         '<div style="margin:5px;">'+
