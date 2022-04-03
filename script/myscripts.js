5f93d1836a7634505ce79758bdf687df52738a67
linih <nightsky257@gmail.com>
2022-04-04 01:28:48 +0800

    first commit

--- script/myscripts.js
+++ script/myscripts.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
     console.log('html');
     $.getJSON('script/data.json', function(data){
         $.each(data, function(key, value){
-            console.log('key=' + key);
-            component_sentence = '<option>'+value[0]+'</option>';
+            //console.log('key=' + key);
+            component_sentence = '<option id="'+key+'</option>';
             $('#select_tradeitem').append(component_sentence);
           });
     })
@@ -13,11 +13,13 @@ $(document).ready(function(){
 $('button#add-item').click(function(){
     var option = $('#select_tradeitem option:selected');
     var option_val = option.val();
+    var option_price = $.getJSON('script/data.json').option_val[1];
     var newdiv =
     '<div class="ui fluid card '+option_val+'">'+
         '<div style="margin:5px;">'+
             option_val+
             '<button id="'+option_val+'" class="mini ui button remove" style="float:right;">刪</button>'+
+            '<div>'+ option_price +'</div>'
         '</div>'+
     '</div>';
 
